04fe7a9afbde2a9fd9f54809221e20e0c84badf9
Samuel A. Yallop <syallop@gmail.com>
2018-04-08 19:58:25 +0100

    Donate Label

--- PLParser/Expected.hs
+++ PLParser/Expected.hs
@@ -26,6 +26,8 @@ module PLParser.Expected
 
 import Prelude hiding (takeWhile,dropWhile,exp)
 
+import PLLabel
+
 import Control.Applicative
 import Control.Monad
 import Data.Char
@@ -42,38 +44,6 @@ data Expected
   | ExpectAnything                         -- Expected anything => got an EOF
   | ExpectN Int Expected                   -- Expected a N repetitions
   | ExpectLabel Label Expected             -- Expected something with a label
-  deriving Show
-
--- | A Label is some textual description of what a Parser expected at a point.
--- The LabelUse dictates whether the label is intended to be descriptive of the
--- entire Parse past this point or is used to enhance existing labels and Parser
--- descriptions.
---
--- When describing why a Parser failed, in general descriptions should stop at
--- the first Descriptive Label and otherwise chain Enhancing labels together.
--- For debugging purposes, one might still want to see all Labels regardless of
--- type.
-data Label = Label
-  { _labelText :: Text
-  , _labelUse  :: LabelUse
-  }
-  deriving Show
-
-descriptiveLabel
-  :: Text
-  -> Label
-descriptiveLabel
-  txt = Label txt Descriptive
-
-enhancingLabel
-  :: Text
-  -> Label
-enhancingLabel
-  txt = Label txt Enhancing
-
-data LabelUse
-  = Descriptive
-  | Enhancing
   deriving Show
 
 expectNothing :: Expected
