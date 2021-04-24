013a0b3a5ceea38a1c5ab6cc4b96c41ef87c6d8d
Samuel A. Yallop <syallop@gmail.com>
2021-04-24 14:44:27 +0100

    Optimise Expected printing for the supplyer of input
    - Optimise the printing logic for Expected, prioritising being useful
      for a person controlling the input to the parser, rather than the
      parser itself.
      - Handle runs of alternatives & sequences
      - Omit printing 'complex' expectations
      - Descriptive labels are sufficient to describe a nested failure. More deeply
        nested expectations are not rendered.
      - Enhancing labels are insufficient to describe nested failure.

--- PLParser/Expected.hs
+++ PLParser/Expected.hs
@@ -44,6 +44,7 @@ import PLLabel
 import PLPrinter
 
 import Data.Function
+import Data.Char
 import Data.Map (Map)
 import Data.Set (Set)
 import Data.Text (Text)
@@ -81,7 +82,226 @@ data Expected
   deriving (Eq, Ord, Show)
 
 instance Document Expected where
-  document = document . show
+  document = documentExpected
+
+-- | Pretty print an 'Expected' that describes why a failure occured
+-- (but not where, or the context).
+--
+-- - Alternatives are rendered as bullet points.
+-- - Text is written literally
+-- - Descriptive Labels are sufficient to describe a nested failure =>
+--   More deeply nested expectations are _not_ rendered.
+-- - Enhancing Labels are insufficient to describe a nested failure.
+--   More deeply nested expectations _are_ rendered.
+documentExpected :: Expected -> Doc
+documentExpected = \case
+  -- TODO:
+  -- This function is _super_ hacky, inefficient and occasionally misleading because
+  -- the underlying structure doesnt map well to visualisation. The type needs
+  -- re-working or the Parser library should be able to create a better
+  -- structured error and perform this fiddly logic.
+
+  -- TODO: Is it better to surround this with quotes (/some other signifier) or
+  -- leave unescaped and potentially make things ambiguous (E.G. labels vs exact
+  -- text)?
+  ExpectText txt
+    -> documentText txt
+
+  -- TODO:
+  -- - Does this make the output noisy? Is this better as mempty?
+  -- - Does this really mean "required input to decide"?
+  ExpectAnything
+    -> text "Any character"
+
+  -- TODO: Should this report or be empty?
+  -- The 'alternatives' function folds with this expectation as a base case.
+  -- Does it makes sense for every alternative to report that it expected a
+  -- failure?
+  -- Is this a rendering mismatch or a bad implementation?
+  ExpectFail
+    -> text "Nothing after exhausting any (and all) other options."
+
+  ExpectEither eLeft eRight
+     -> let runs = List.nub . eitherRuns . ExpectEither eLeft $ eRight
+            (simple,_complex) = categorise runs
+         in mconcat
+              [ bulleted . fmap documentExpected $ simple
+              , lineBreak
+
+              --, text "Or less simple alternatives:"
+              --, bulleted . fmap ppExpected $ complex
+              --, lineBreak
+              ]
+
+  -- If a Label is descriptive, we don't need to describe further.
+  ExpectLabel (Label label Descriptive) _e
+    | label == ""
+    -> mempty -- TODO: Should this be interpreted as a request to hide really?
+
+    | otherwise
+    -> documentText label
+
+  -- If a Label is enhancing, we'd still like to see the rest of the
+  -- Expectation.
+  ExpectLabel (Label label Enhancing) e
+    | label == ""
+    -> mempty
+
+    | otherwise
+    -> mconcat
+         [ documentText label
+         , text ":"
+         , lineBreak
+         , indent 3 . documentExpected $ e
+         ]
+
+  -- If a predicate Label is descriptive, we don't need to describe further.
+  ExpectPredicate (Label label Descriptive) _
+    | label == ""
+    -> mempty -- TODO: Should this be interpreted as a request to hide really?
+
+    | otherwise
+    -> mconcat
+         [ documentText label
+         , text "?"
+         ]
+
+  -- If a predicate Label is enhancing, we'd still like to see the rest of the
+  -- Expectation.
+  ExpectPredicate (Label label Enhancing) mE
+    | label == ""
+    -> mempty -- TODO: Should this be interpreted as a request to hide really?
+
+    | otherwise
+    -> mconcat
+         [ documentText label
+         , text "?"
+         , maybe mempty
+                 (\e -> mconcat [ lineBreak
+                                , indent 2 . documentExpected $ e
+                                ]
+                 )
+                 mE
+         ]
+
+  ExpectN n e
+    -> mconcat
+         [ text . Text.pack . show $ n
+         , text " times:"
+         , indent 2 . documentExpected $ e
+         ]
+
+
+  ExpectThen eFirst eThen
+    -> mconcat
+         [ text "In order:"
+         , lineBreak
+         , bulleted . filter ((/= "") . render) . fmap documentExpected . thenRuns . ExpectThen eFirst $ eThen
+         ]
+
+  where
+    -- Break out nested runs of eithers.
+    -- E.G. (a | b) | (c | d) = a | b | c | d
+    eitherRuns :: Expected -> [Expected]
+    eitherRuns = \case
+      ExpectEither eLeft eRight
+        -> eitherRuns eLeft <> eitherRuns eRight
+
+      e -> [e]
+
+    -- Break out nested runs of thens.
+    -- E.G. (a; b); (c; d) = a; b; c; d
+    thenRuns :: Expected -> [Expected]
+    thenRuns = \case
+      ExpectThen eFirst eThen
+        -> thenRuns eFirst <> thenRuns eThen
+
+      e -> [e]
+
+    -- Categorise into 'simple' and 'complex'.
+    -- Things like expecting literal text, or labels wrapping other simple
+    -- things are simple.
+    --
+    -- Anything else is complex.
+    categorise :: [Expected] -> ([Expected], [Expected])
+    categorise es = let (simple,complex) = foldl (\(accSimple, accComplex) e
+                                                   -> if isSimple e
+                                                        then (e:accSimple, accComplex)
+                                                        else (accSimple, e:accComplex)
+                                                 )
+                                                 ([], [])
+                                                 es
+                     in (reverse simple, reverse complex)
+      where
+        isSimple :: Expected -> Bool
+        isSimple = \case
+          ExpectText _
+            -> True
+          ExpectLabel (Label _ Descriptive) _
+            -> True
+          ExpectLabel (Label _ Enhancing) e
+            -> isSimple e
+          ExpectPredicate (Label _ Descriptive) _
+            -> True
+          ExpectPredicate (Label _ Enhancing) Nothing
+            -> True
+          ExpectPredicate (Label _ Enhancing) (Just e)
+            -> isSimple e
+
+
+          ExpectN _ _
+            -> False
+          ExpectAnything
+            -> False
+          ExpectFail
+            -> False
+          ExpectEither _ _
+            -> False
+          ExpectThen _ _
+            -> False
+
+-- TODO: Belongs in PLPrinter?
+
+-- | Pretty Print text that may contain non-printable characters.
+--
+-- If a non-printable character is detected, use escape sequences as given by
+-- 'show' but do _not_ surround the result in quotation marks.
+--
+-- We are therefore making output ambiguous and assuming users can infer from
+-- context...
+documentText :: Text -> Doc
+documentText txt
+  | Text.all isPrint txt
+  = text txt
+
+  | otherwise
+   = text . Text.pack . showUnquoted . Text.unpack $ txt
+  where
+    -- Show without quotes
+    --
+    -- Plain 'show':
+    -- - Has no quotes for simple characters. E.G.
+    --   a
+    --   1
+    --   @
+    -- - Uses escape sequences for other characters, but also surrounds them
+    --   with quotes. E.G.
+    --   "\n"
+    --   "\t"
+
+    -- If a 'Show'able thing is rendered with surrounding quotes, drop them.
+    showUnquoted :: Show a => a -> String
+    showUnquoted = showUnquoted' . show
+
+    showUnquoted' :: String -> String
+    showUnquoted' s@[_]                     = s
+    showUnquoted' ('"':s)  | last s == '"'  = init s
+                           | otherwise      = s
+    showUnquoted' ('\'':s) | last s == '\'' = init s
+                           | otherwise      = s
+    showUnquoted' s                         = s
+
+
 
 -- | We have no expectations and therefore expect to fail.
 expectNothing :: Expected
